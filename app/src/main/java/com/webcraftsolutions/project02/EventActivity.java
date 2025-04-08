b5a6fd0af18bb798f200bc795358ec02db97bde2
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-08 10:44:29 -0700

    EventActivity.java now uses binding object.

--- app/src/main/java/com/webcraftsolutions/project02/EventActivity.java
+++ app/src/main/java/com/webcraftsolutions/project02/EventActivity.java
@@ -10,17 +10,17 @@ package com.webcraftsolutions.project02;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.webcraftsolutions.project02.databinding.ActivityEventBinding;
 
 public class EventActivity extends AppCompatActivity {
 
     // CLASS FIELDS
 
     // INSTANCE FIELDS
+
+    private ActivityEventBinding binding;
 
     // METHODS
 
@@ -35,13 +35,10 @@ public class EventActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_event);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Set binding
+        binding = ActivityEventBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     // STATIC METHODS
