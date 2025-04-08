faf509023e699b2fb710643920639d41782bd518
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-08 10:32:04 -0700

    Added comments to EventActivity.java.

--- app/src/main/java/com/webcraftsolutions/project02/EventActivity.java
+++ app/src/main/java/com/webcraftsolutions/project02/EventActivity.java
@@ -1,3 +1,11 @@
+/**
+ * Title: Project 02: Activiti - Event Activity
+ * Author: Noah deFer
+ * Date Created: 4/8/2025
+ * Description: The main activity for the Events section of the application.
+ *      Will allow users to view, create, edit, and delete events.
+ */
+
 package com.webcraftsolutions.project02;
 
 import android.os.Bundle;
@@ -10,6 +18,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EventActivity extends AppCompatActivity {
 
+    // CLASS FIELDS
+
+    // INSTANCE FIELDS
+
+    // METHODS
+
+    /**
+     * Called when this activity is created.
+     * Sets an OnClickListener for each button.
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,4 +43,8 @@ public class EventActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    // STATIC METHODS
+
+    // GETTERS AND SETTERS
 }
