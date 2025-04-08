8aa3f232ad162991c05e7efd211749a1a3247c3e
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-08 10:10:47 -0700

    MainActivity.java now uses binding object.

--- app/src/main/java/com/webcraftsolutions/project02/MainActivity.java
+++ app/src/main/java/com/webcraftsolutions/project02/MainActivity.java
@@ -18,11 +18,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.webcraftsolutions.project02.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     // CLASS FIELDS
 
     // INSTANCE FIELDS
+
+    private ActivityMainBinding binding;
 
     // METHODS
 
@@ -37,13 +41,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Set binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // TODO Set OnClickListener for logoutButton
 
