7d02e0aff7a6fc256b864154202e133dbb80ae5e
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-09 15:53:43 -0700

    Added comments to ActivitiDatabase.java. Added some fields and RoomDatabase statements as well.

--- app/src/main/java/com/webcraftsolutions/project02/database/ActivitiDatabase.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/ActivitiDatabase.java
@@ -1,4 +1,32 @@
+/**
+ * Title: Project 02: Activiti - Activiti Database
+ * @author Noah deFer
+ * Date Created: 4/9/2025
+ * Description: Database object for Activiti databse.
+ */
 package com.webcraftsolutions.project02.database;
 
-public class ActivitiDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.webcraftsolutions.project02.database.entities.Event;
+
+@Database(entities = {Event.class}, version = 1, exportSchema = false)
+public abstract class ActivitiDatabase extends RoomDatabase {
+
+    // CLASS FIELDS
+
+    // This ActivitiDatabase instance
+    public static ActivitiDatabase INSTANCE;
+
+    // ID for the database
+    private static final String DATABASE_NAME = "Activiti_Database";
+
+    // ID for event table
+    public static final String EVENT_TABLE = "Event_Table";
+
+    // CONSTRUCTOR
+
+    // METHODS
+
 }
