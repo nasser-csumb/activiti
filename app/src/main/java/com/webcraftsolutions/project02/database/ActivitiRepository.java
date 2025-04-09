63a186afc62ae86f7be27e414aa881c696550b19
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-09 16:23:57 -0700

    Added fields to ActivitiRepository.java.

--- app/src/main/java/com/webcraftsolutions/project02/database/ActivitiRepository.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/ActivitiRepository.java
@@ -6,10 +6,24 @@
  */
 package com.webcraftsolutions.project02.database;
 
+import com.webcraftsolutions.project02.database.entities.Event;
+
+import java.util.ArrayList;
+
 public class ActivitiRepository {
     // CLASS FIELDS
 
+    // Stores the repository instance.
+    private static ActivitiRepository repository;
+
     // INSTANCE FIELDS
+
+    // Stores the EventDAO instance.
+
+    // Stores an array of Event instances.
+    private ArrayList<Event> allEventLogs;
+
+    private final EventDAO eventDAO;
 
     // CONSTRUCTORS
 
