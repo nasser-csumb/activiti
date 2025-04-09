3426d5c3bc36c460e3be302d5deb639e6cb402d9
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-09 16:06:54 -0700

    Setup ActivitiDatabase.java.

--- app/src/main/java/com/webcraftsolutions/project02/database/ActivitiDatabase.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/ActivitiDatabase.java
@@ -6,7 +6,10 @@
  */
 package com.webcraftsolutions.project02.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.webcraftsolutions.project02.database.entities.Event;
@@ -25,8 +28,34 @@ public abstract class ActivitiDatabase extends RoomDatabase {
     // ID for event table
     public static final String EVENT_TABLE = "Event_Table";
 
-    // CONSTRUCTOR
+    // ID for user table
+    public static final String USER_TABLE = "User_Table";
 
     // METHODS
+    /**
+     * Returns a GymLogDatabase instance.
+     * Creates a new instance only if one doesn't exist.
+     * @param context The application context.
+     * @return A GymLogDatabase instance.
+     */
+    //TODO Enable add addCallback call once addDefaultValues is added.
+    static ActivitiDatabase getDatabase(final Context context) {
+        if(INSTANCE == null) {
+            synchronized (ActivitiDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            ActivitiDatabase.class,
+                            DATABASE_NAME)
+                            .fallbackToDestructiveMigration()
+//                            .addCallback(addDefaultValues)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    // TODO Add addDefaultValues method here after User and UserDAO are setup.
 
 }
