caf9fffc70779f26562397737d90ea645e0e1a1b
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-09 16:35:34 -0700

    Added ExecutorService fields to ActivitiDatabase.java.

--- app/src/main/java/com/webcraftsolutions/project02/database/ActivitiDatabase.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/ActivitiDatabase.java
@@ -14,10 +14,19 @@ import androidx.room.RoomDatabase;
 
 import com.webcraftsolutions.project02.database.entities.Event;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {Event.class}, version = 1, exportSchema = false)
 public abstract class ActivitiDatabase extends RoomDatabase {
 
     // CLASS FIELDS
+
+    // The number of threads used by the ExecutorService
+    private static final int NUMBER_OF_THREADS = 4;
+
+    // The ExecutorService instance.
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
     // This ActivitiDatabase instance
     public static ActivitiDatabase INSTANCE;
