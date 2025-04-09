2d7a328b311deba9541c9b96af6ceea37fb89200
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-09 16:16:30 -0700

    Setup EventDAO.java.

--- app/src/main/java/com/webcraftsolutions/project02/database/EventDAO.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/EventDAO.java
@@ -1,4 +1,30 @@
+/**
+ * Title: Project 02: Activiti - Event DAO
+ * @author Noah deFer
+ * Date Created: 4/9/2025
+ * Description: DAO for event table.
+ */
 package com.webcraftsolutions.project02.database;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.webcraftsolutions.project02.database.entities.Event;
+
+@Dao
 public interface EventDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Event... event);
+
+    @Delete
+    void delete(Event event);
+
+    @Query("SELECT * FROM " + ActivitiDatabase.EVENT_TABLE)
+    Event getAllEvents();
+
+    @Query("SELECT * FROM " + ActivitiDatabase.EVENT_TABLE + " WHERE userId == :userId")
+    Event getAllEventsByUserId(int userId);
 }
