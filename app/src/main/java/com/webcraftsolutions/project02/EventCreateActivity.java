ac1efe213a646250f165caebec2997b8903a56cc
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-09 10:42:29 -0700

    EventCreateActivity.java now uses binding object.

--- app/src/main/java/com/webcraftsolutions/project02/EventCreateActivity.java
+++ app/src/main/java/com/webcraftsolutions/project02/EventCreateActivity.java
@@ -2,23 +2,20 @@ package com.webcraftsolutions.project02;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.webcraftsolutions.project02.databinding.ActivityEventCreateBinding;
 
 public class EventCreateActivity extends AppCompatActivity {
+
+    private ActivityEventCreateBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_event_create);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Get binding
+        binding = ActivityEventCreateBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
