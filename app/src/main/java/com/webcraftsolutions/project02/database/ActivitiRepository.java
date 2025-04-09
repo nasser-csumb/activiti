7f9e1d83f276dec9de1b3f5a2de77dfe2cbb4d02
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-09 16:21:16 -0700

    Added comments to ActivitiRepository.java.

--- app/src/main/java/com/webcraftsolutions/project02/database/ActivitiRepository.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/ActivitiRepository.java
@@ -1,4 +1,17 @@
+/**
+ * Title: CST-338 Project 02: Activiti - Activiti Repository
+ * @author Noah deFer
+ * Date Created: 4/9/2025
+ * Description: Repository class for Activiti database.
+ */
 package com.webcraftsolutions.project02.database;
 
 public class ActivitiRepository {
+    // CLASS FIELDS
+
+    // INSTANCE FIELDS
+
+    // CONSTRUCTORS
+
+    // METHODS
 }
