ad5b0efb7740337e90eace163e865c6f1411425e
JianMitchell <jimitchell@csumb.edu>
2025-04-13 20:29:52 -0700

    Initial implementation of TravelExploration.java

---
+++ app/src/main/java/com/webcraftsolutions/project02/database/entities/TravelExploration.java
@@ -0,0 +1,64 @@
+package com.webcraftsolutions.project02.database.entities;
+
+import androidx.room.PrimaryKey;
+import androidx.room.Entity;
+
+import com.webcraftsolutions.project02.database.ActivitiDatabase;
+
+@Entity(tableName = ActivitiDatabase.TravelExploration_Table)
+public class TravelExploration {
+
+    @PrimaryKey(autoGenerate = true)
+    private int travelID;
+    private String destination;
+    private String startDate;
+    private String endDate;
+    private String description;
+
+    public TravelExploration(String destination, String startDate, String endDate, String description) {
+        this.destination = destination;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+
+    public int getTravelID() {
+        return travelID;
+    }
+
+    public void setTravelID(int travelID) {
+        this.travelID = travelID;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
