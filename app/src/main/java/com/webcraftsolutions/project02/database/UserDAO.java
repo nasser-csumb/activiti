7b77d122c4497e4242e1c1fc748ed6774544bdbd
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-15 09:54:47 -0700

    Setup UserDAO.java.

--- app/src/main/java/com/webcraftsolutions/project02/database/UserDAO.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/UserDAO.java
@@ -1,4 +1,38 @@
+/**
+ * Title: CST-338 Project 02: Activiti - User DAO
+ * @author Noah deFer
+ * Date Created: 4/15/2025
+ * Description: Data access object for the user entitity.
+ */
 package com.webcraftsolutions.project02.database;
 
-public class UserDAO {
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.webcraftsolutions.project02.database.entities.User;
+
+@Dao
+public interface UserDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(User... user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("SELECT * FROM " + ActivitiDatabase.USER_TABLE)
+    LiveData<User> getAllUsers();
+
+    @Query("DELETE FROM " + ActivitiDatabase.USER_TABLE)
+    void deleteAll();
+
+    @Query("SELECT * FROM " + ActivitiDatabase.USER_TABLE + " WHERE username == :username")
+    LiveData<User> getUserByUserName(String username);
+
+    @Query("SELECT * FROM " + ActivitiDatabase.USER_TABLE + " WHERE id == :userId")
+    LiveData<User> getUserByUserId(int userId);
 }
