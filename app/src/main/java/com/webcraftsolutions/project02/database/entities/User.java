b8d20f4aac8d95b9b2320ced89f8f6dce4f4b3c3
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-15 09:43:50 -0700

    Setup User.java.

--- app/src/main/java/com/webcraftsolutions/project02/database/entities/User.java
+++ app/src/main/java/com/webcraftsolutions/project02/database/entities/User.java
@@ -1,4 +1,79 @@
+/**
+ * Title: CST-338 Project 02: Activiti - User Entity
+ * @author Noah deFer
+ * Date: 4/15/2025
+ * Description: A POJO that stores user data.
+ */
 package com.webcraftsolutions.project02.database.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.webcraftsolutions.project02.database.ActivitiDatabase;
+
+import java.util.Objects;
+
+@Entity(tableName = ActivitiDatabase.USER_TABLE)
 public class User {
+    // FIELDS
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    // CONSTRUCTOR
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        isAdmin = false;
+    }
+
+    // GETTERS & SETTERS
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    // EQUALS & HASHCODE
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && isAdmin == user.isAdmin && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, isAdmin);
+    }
 }
