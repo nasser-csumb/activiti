15e4909499e51a22cceddf8d4f97b9263493c8d8
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-15 11:17:56 -0700

    Added OnClickListeners, repository to LoginActivity.java.

--- app/src/main/java/com/webcraftsolutions/project02/LoginActivity.java
+++ app/src/main/java/com/webcraftsolutions/project02/LoginActivity.java
@@ -6,10 +6,13 @@
  */
 package com.webcraftsolutions.project02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.webcraftsolutions.project02.database.ActivitiRepository;
 import com.webcraftsolutions.project02.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,7 +20,10 @@ public class LoginActivity extends AppCompatActivity {
     // INSTANCE FIELDS
 
     // The binding object for LoginActivity
-    ActivityLoginBinding binding;
+    private ActivityLoginBinding binding;
+
+    // The repository
+    private ActivitiRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,38 @@ public class LoginActivity extends AppCompatActivity {
         // Get Binding
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Get repository
+        repository = ActivitiRepository.getRepository(getApplication());
+
+        // Set OnClickListener for Login Button
+        binding.loginLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+
+        /*
+        Set OnLongClickListener for Login Button
+        Logs in admin without having to type login credentials, FOR DEBUGGING.
+        */
+        binding.loginLoginButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext());
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        // Set OnClickListener for SignUp Button
+        binding.loginSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
