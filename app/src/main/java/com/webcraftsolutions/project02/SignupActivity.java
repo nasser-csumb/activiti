f3eaa60a1b6217d576c360c8723342ff6d7047c1
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-16 11:13:06 -0700

    SignupActivity.java now uses binding object.

--- app/src/main/java/com/webcraftsolutions/project02/SignupActivity.java
+++ app/src/main/java/com/webcraftsolutions/project02/SignupActivity.java
@@ -12,18 +12,32 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.webcraftsolutions.project02.databinding.ActivitySignupBinding;
+
 public class SignupActivity extends AppCompatActivity {
 
     // CLASS FIELDS
 
     // INSTANCE FIELDS
 
+    // The binding object.
+    ActivitySignupBinding binding;
+
     // INSTANCE METHODS
 
+    /**
+     * Called when the activity is created.
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+
+        // Get binding
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     // STATIC METHODS
