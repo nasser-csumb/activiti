2225faf5a561dfd444e0cf82dc6fa9e1e95f4d34
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-16 11:09:05 -0700

    Added header and comments.

--- app/src/main/java/com/webcraftsolutions/project02/SignupActivity.java
+++ app/src/main/java/com/webcraftsolutions/project02/SignupActivity.java
@@ -1,3 +1,11 @@
+/**
+ * Title: CST-338 Project 02: Activiti - Signup Page
+ * @author Noah deFer
+ * Date Created: 4/16/2025
+ * Description: An activity to allow the user to signup for Activiti.
+ *      New user credentials will be added to the User table.
+ */
+
 package com.webcraftsolutions.project02;
 
 import android.os.Bundle;
@@ -6,9 +14,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
+    // CLASS FIELDS
+
+    // INSTANCE FIELDS
+
+    // INSTANCE METHODS
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
+
+    // STATIC METHODS
 }
