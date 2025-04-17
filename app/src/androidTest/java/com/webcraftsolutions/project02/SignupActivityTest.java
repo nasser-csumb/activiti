c475dd1df0e71bfb2b6a30fd47dce12a6733ea10
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-17 14:49:26 -0700

    Setup SignupActivityTest.java.

--- app/src/androidTest/java/com/webcraftsolutions/project02/SignupActivityTest.java
+++ app/src/androidTest/java/com/webcraftsolutions/project02/SignupActivityTest.java
@@ -2,11 +2,41 @@ package com.webcraftsolutions.project02;
 
 import static org.junit.Assert.*;
 
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SignupActivityTest {
 
+    // Test String
+    String SIGNUP_EXTRA = "SIGNUP_INTENT_EXTRA";
+
+    // Application Context
+    Context context;
+
+    @Before
+    public void setUp() throws Exception {
+        // Get Context
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        context = null;
+    }
+
     @Test
     public void signupActivitiIntentFactory() {
+        // Get Intent
+        Intent intent = SignupActivity.signupActivitiIntentFactory(context);
+
+        // Check Intent
+        assertNotNull(intent);
+        assertFalse(intent.getBooleanExtra(SIGNUP_EXTRA, false));
     }
 }
