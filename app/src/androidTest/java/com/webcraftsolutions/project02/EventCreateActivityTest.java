e0da2f0ec6a2e1890e7dd514c33db83603df4804
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-17 14:57:10 -0700

    Created EventCreateActivityTest.java.

---
+++ app/src/androidTest/java/com/webcraftsolutions/project02/EventCreateActivityTest.java
@@ -0,0 +1,22 @@
+package com.webcraftsolutions.project02;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventCreateActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void eventCreateActivityIntentFactory() {
+    }
+}
