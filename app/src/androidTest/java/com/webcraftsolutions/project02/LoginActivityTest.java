47edf794a06e884ae2ee83c2141ae94b398a7c60
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-17 14:40:23 -0700

    Setup LoginActivityTest.java.

--- app/src/androidTest/java/com/webcraftsolutions/project02/LoginActivityTest.java
+++ app/src/androidTest/java/com/webcraftsolutions/project02/LoginActivityTest.java
@@ -8,21 +8,42 @@ package com.webcraftsolutions.project02;
 
 import static org.junit.Assert.*;
 
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LoginActivityTest {
 
+    // Test String
+    String LOGIN_EXTRA = "LOGIN_INTENT_EXTRA";
+
+    // Application Context
+    Context CONTEXT;
+
     @Before
     public void setUp() throws Exception {
+        // Get Context
+        CONTEXT = InstrumentationRegistry.getInstrumentation().getTargetContext();
     }
 
     @After
     public void tearDown() throws Exception {
+        // Invalidate Variables
+        CONTEXT = null;
     }
 
     @Test
     public void loginActivityIntentFactory() {
+        // Get Intent
+        Intent intent = LoginActivity.loginActivityIntentFactory(CONTEXT);
+
+        // Check Intent
+        assertNotNull(intent);
+        assertFalse(intent.getBooleanExtra(LOGIN_EXTRA, false));
     }
 }
