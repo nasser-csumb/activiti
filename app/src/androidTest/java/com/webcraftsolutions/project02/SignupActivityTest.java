b9b537a2d8a967167236fd62bdaeb7f47cbbc3f9
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-17 14:40:50 -0700

    Created SignupActivityTest.java.

---
+++ app/src/androidTest/java/com/webcraftsolutions/project02/SignupActivityTest.java
@@ -0,0 +1,12 @@
+package com.webcraftsolutions.project02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SignupActivityTest {
+
+    @Test
+    public void signupActivitiIntentFactory() {
+    }
+}
