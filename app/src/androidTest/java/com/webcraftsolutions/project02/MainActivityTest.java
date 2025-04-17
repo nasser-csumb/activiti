a5c78c1764e7d6df3aa28573d2f7659cb5a8adbc
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-17 14:25:22 -0700

    Finished MainActivityTest.java intent factory tests.

--- app/src/androidTest/java/com/webcraftsolutions/project02/MainActivityTest.java
+++ app/src/androidTest/java/com/webcraftsolutions/project02/MainActivityTest.java
@@ -5,22 +5,47 @@ import android.content.Intent;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.webcraftsolutions.project02.database.entities.User;
+
+import junit.framework.Test;
 import junit.framework.TestCase;
 
 public class MainActivityTest extends TestCase {
 
+    // Test Admin User
+    User TEST_ADMIN;
+    String TEST_ADMIN_NAME = "TEST_ADMIN";
+
+    // Test User
+    User TEST_USER;
+    String TEST_USER_NAME = "TEST_USER";
+
     // Application Context
     Context CONTEXT;
+
+    // Logout Boolean
+    boolean LOGOUT;
 
     public void setUp() throws Exception {
         super.setUp();
 
         // Get Application Context
         CONTEXT = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        // Set Test Users
+        TEST_ADMIN = new User(TEST_ADMIN_NAME, TEST_ADMIN_NAME);
+        TEST_ADMIN.setAdmin(true);
+        TEST_USER = new User(TEST_USER_NAME, TEST_USER_NAME);
+
+        // Set Logout Boolean
+        LOGOUT = true;
     }
 
     public void tearDown() throws Exception {
         super.tearDown();
+
+        // Invalidate Variables
+        CONTEXT = null;
     }
 
     public void testDeleteUser() {
@@ -31,14 +56,22 @@ public class MainActivityTest extends TestCase {
 
     public void testMainActivityIntentFactoryUserId() {
         // Get Intent
-        Intent intent = MainActivity.mainActivityIntentFactory(CONTEXT, 1);
+        Intent intent = MainActivity.mainActivityIntentFactory(CONTEXT, TEST_USER.getId());
 
         // Check Intent Extra
-        assertEquals(1,
-                intent.getIntExtra(MainActivity.LOGGED_IN_USER_ID_KEY, MainActivity.LOGGED_OUT));
+        assertEquals(TEST_USER.getId(), intent.getIntExtra(
+                MainActivity.LOGGED_IN_USER_ID_KEY, MainActivity.LOGGED_OUT));
+        assertEquals(MainActivity.LOGGED_OUT, intent
+                .getIntExtra("", MainActivity.LOGGED_OUT));
 
     }
 
     public void testMainActivityIntentFactoryLogout() {
+        // Get Intent
+        Intent intent = MainActivity.mainActivityIntentFactory(CONTEXT, LOGOUT);
+
+        // Check Intent Extra
+        assertEquals(LOGOUT, intent.getBooleanExtra(MainActivity.USER_LOGOUT_KEY, !LOGOUT));
+        assertEquals(!LOGOUT, intent.getBooleanExtra("", !LOGOUT));
     }
 }
